8b6aa0834060d12f13a3a281632cee4e323bf16a
jajaa17 <judielhipolito@gmail.com>
2024-10-16 08:18:21 +0800

    feat: added navbar logo

--- app/Views/components/navbar.php
+++ app/Views/components/navbar.php
@@ -3,6 +3,9 @@
 <section>
     <nav class="navbar navbar-expand-lg bg-body-tertiary rounded" aria-label="Eleventh navbar example">
       <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+        <img src="<?php echo IMG.'blackLogo.png';?>" alt="" width="64" height="64">
+        </a>
         <div class="navbar-collapse collapse" id="navbarsExample09">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
